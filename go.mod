d5f87d689dcd2e33b92ff98523fb2e04d83ac4e3
alsamorodov <alsamorodov@ozon.ru>
2024-05-01 16:05:00 +0300

    add get command with recover from panic with defer construction like finally

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/alexeysamorodov/bot
 
 go 1.20
 
-require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
-
-require github.com/joho/godotenv v1.4.0 // indirect
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/joho/godotenv v1.4.0
+)
--- internal/app/commands/commander.go
+++ internal/app/commands/commander.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"log"
+
 	"github.com/alexeysamorodov/bot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -20,6 +22,12 @@ func NewCommander(bot *tgbotapi.BotAPI, productService *product.Service) *Comman
 }
 
 func (c *Commander) HandleUpdate(update *tgbotapi.Update) {
+	defer func() {
+		if panicValue := recover(); panicValue != nil {
+			log.Printf("Recovered from panic: %v", panicValue)
+		}
+	}()
+
 	if update.Message == nil {
 		return
 	}
